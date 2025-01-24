51f3ddcde7c8526f953b02142d6ec18dba25c887
Asikuzzaman Sojib <zasik480@gmail.com>
2025-01-24 23:04:15 +0600

    Add files via upload

---
+++ TRTOV.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(void){
+
+    const float revenue_per_150 = 4.5f;
+    short janSold = 23500;
+    short febSold = 19300;
+    short marSold = 21600;
+    
+
+    return 0;
+}
